27ae366354f794879d71932b4f59e263c9ef15ec
Mati <mati@ratio.team>
2019-01-11 11:39:09 +0100

    magento removed from gitignore

---
+++ web/js/storefront/index.js
@@ -0,0 +1,42 @@
+import React from "./react";
+import EditorialIntroNike from "ecommerce/src/helpers/Editorial/EditorialIntroNike/EditorialIntroNike";
+
+// import MenuBarNikeStory from "ecommerce/src/helpers/MenuBarNike/MenuBarNike.story";
+// import ProductWrapper from "ecommerce/src/helpers/ProductWrapper/ProductWrapper";
+// import {ColumnLayoutStoryNike} from "ecommerce/src/core/ColumnLayout/ColumnLayout.story";
+// import {
+//     EditorialBigImageStoryNike,
+//     EditorialBigImageStoryNike2, EditorialBigImageStoryNike3
+// } from "ecommerce/src/core/Editorial/EditorialBigImage/EditorialBigImage.story";
+// import EditorialIntroNikeStory from "ecommerce/src/helpers/Editorial/EditorialIntroNike/EditorialIntroNike.story";
+// import EditorialIntroNike from "ecommerce/src/helpers/Editorial/EditorialIntroNike/EditorialIntroNike";
+// import {AgileGridStoryNike} from "ecommerce/src/core/AgileGrid/AgileGrid.story";
+// import FooterNikeStory from "ecommerce/src/helpers/FooterNike/FooterNike.story";
+
+class ShoppingList extends React.Component {
+    render() {
+
+        {/*<ProductWrapper>*/}
+        {/*{MenuBarNikeStory}*/}
+        {/*{ColumnLayoutStoryNike}*/}
+
+        {/*{EditorialBigImageStoryNike}*/}
+
+        {/*{EditorialIntroNikeStory}*/}
+
+        {/*{EditorialBigImageStoryNike2}*/}
+        {/*{EditorialBigImageStoryNike3}*/}
+
+        {/*<EditorialIntroNike subTitle={"You may also like"} />*/}
+
+        {/*{AgileGridStoryNike}*/}
+        {/*{FooterNikeStory}*/}
+        {/*</ProductWrapper>*/}
+
+        return (
+            <EditorialIntroNike />
+        );
+    }
+}
+
+global.ShoppingList = ShoppingList;
